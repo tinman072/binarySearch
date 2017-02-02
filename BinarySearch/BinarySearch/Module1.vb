97b300cffa1d56169c7ee92d6fa6058a60a31652
tinman072 <nahyan.quasem+github@gmail.com>
2017-02-02 20:32:17 +0600

    Add files via upload

---
+++ BinarySearch/BinarySearch/Module1.vb
@@ -0,0 +1,62 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim numbers() As Integer = {2, 3, 5, 7, 11, 13, 17, 19, 23}
+
+        'Dim numbers(9) As Integer
+
+        'numbers(0) = 2
+        'numbers(1) = 3
+        'numbers(2) = 5
+        'numbers(3) = 7
+        'numbers(4) = 11
+        'numbers(5) = 13
+        'numbers(6) = 17
+        'numbers(7) = 19
+        'numbers(8) = 23
+
+
+        Dim key As Integer
+
+        key = 5
+
+        Dim found As Boolean
+
+        found = False
+
+        Dim l, m, h As Integer
+
+        l = 0
+        h = 8
+
+        m = Int(((l + h) / 2))
+
+
+        While (l < h)
+            If numbers(m) = key Then
+                found = True
+                Console.WriteLine("the key was found at index " & m)
+                Exit While
+            End If
+
+            If numbers(m) > key Then
+                h = m - 1
+            Else
+                l = m + 1
+            End If
+
+            m = Int((l + h) / 2)
+
+
+        End While
+
+        If found = False Then
+            Console.WriteLine("the number was not found")
+        End If
+
+        'Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ BinarySearch/BinarySearch/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BinarySearch")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BinarySearch")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aacce68d-de1b-4722-950e-be61365fa5b6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
